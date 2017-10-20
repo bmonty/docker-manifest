2e61bd5a873a7b5c525cde930f86ae6ac0549f2b
Benjamin Montgomery <bmontgom@montynet.org>
2017-10-20 06:06:06 -0500

    refactor getAuthenticationInfo

--- docker-manifest.js
+++ docker-manifest.js
@@ -14,41 +14,85 @@ function nonSecureRequest(url, options) {
 /**
  * Query registry server for authentication info.
  *
- * Response looks like: Www-Authenticate: Bearer realm="https://registry-auth",service="repository"
+ * @private
+ * @param {string} registryUrl URL for the registry to get authentication info.
+ * @param {Object} parsedImage Object with the parsed image name
+ * @param {Object} options Options for axios to use
+ * @returns {Object} Authentication info for the requested registry and image.
+ * @throws Will throw an error if authentication info can't be parsed or the realm is unreachable.
  */
-function getAuthenticationInfo(repoUrl, nonSecure = false) {
-  let request;
-  const options = {
+async function getAuthenticationInfo(image, options = null) {
+  // parse image string to determine registry url
+  let registryUrl = '';
+  const parsedImage = parse(image);
+  if (parsedImage.registry) {
+    registryUrl = `https://${parsedImage.registry}/v2/`;
+  } else {
+    registryUrl = 'https://registry-1.docker.io/v2/';
+  }
+
+  // options passed to axios
+  const queryOptions = {
+    ...options.requestOptions,
     validateStatus: status => status === 401,
   };
 
-  if (nonSecure) {
-    request = nonSecureRequest(repoUrl, options);
-  } else {
-    request = axios.get(repoUrl, options);
+  // add authentication info if provided
+  if (options.auth) {
+    queryOptions.auth = options.auth;
   }
 
-  return request
-    .then((response) => {
-      if (response.headers['www-authenticate']) {
-        const re = /Bearer realm="(.*)",service="(.*)"/i;
-        const found = response.headers['www-authenticate'].match(re);
-        if (found) {
-          return {
-            realm: found[1],
-            service: found[2],
-          };
-        }
+  try {
+    // await axios get request, this will be a 401 response and the registry
+    // will provide info required to get an authentication token
+    const realmResponse = await axios.get(registryUrl, queryOptions);
+
+    // parse auth response for the realm and service params provided by
+    // registry
+    const authInfo = {};
+    if (realmResponse.headers['www-authenticate']) {
+      const re = /Bearer realm="(.*)",service="(.*)"/i;
+      const found = realmResponse.headers['www-authenticate'].match(re);
+      if (found) {
+        [, authInfo.realm, authInfo.service] = found;
       }
-      throw new Error('Server did not provide authentication information.');
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    } else {
+      throw new Error("Can't get authentication info for registry.");
+    }
+
+    // use the realm and service info to get an auth token from the registry
+    const tokenOptions = {
+      ...options.requestOptions,
+      params: {
+        service: authInfo.service,
+        scope: `repository:${parsedImage.namespace ? `${parsedImage.namespace}/` : 'library/'}${parsedImage.repository}:pull`,
+      },
+    };
+
+    // add authentication info if provided
+    if (options.auth) {
+      tokenOptions.auth = options.auth;
+    }
+
+    // await axios get request, response should contain the auth token
+    const tokenResponse = await axios.get(authInfo.realm, tokenOptions);
+
+    if (!tokenResponse.data.token) {
+      throw new Error("Can't get authentication token from registry.");
+    }
+
+    return {
+      ...authInfo,
+      token: tokenResponse.data.token,
+    };
+  } catch (error) {
+    console.log('in getAuthenticationInfo: ', error.message);
+  }
 }
 
 /**
  * Query the registry server for the manifest info on a docker image.
+ * @private
  */
 function getManifest(options) {
   const authOptions = {};
@@ -89,41 +133,19 @@ function getManifest(options) {
     });
 }
 
-function getImageManifest(image, username = null, password = null, nonSecure = false) {
-  let repoUrl = '';
-  const parsedImage = parse(image);
-  if (parsedImage.registry) {
-    repoUrl = `https://${parsedImage.registry}/v2/`;
-  } else {
-    repoUrl = 'https://registry-1.docker.io/v2/';
-  }
+/**
+ * Get manifest info for a docker image.
+ * @param {string} image The name of the image. Example: "alpine:latest".
+ * @param {Object} options Request options passed to axios.
+ */
+async function getImageManifest(image, options = null) {
+  // get an oauth token for the registry
+  const authInfo = await getAuthenticationInfo(image, options);
+  console.log(authInfo);
 
-  return getAuthenticationInfo(repoUrl, true)
-    .then((authInfo) => {
-      const options = {
-        token: {
-          url: authInfo.realm,
-          params: {
-            service: authInfo.service,
-            scope: `repository:${parsedImage.namespace ? `${parsedImage.namespace}/` : 'library/'}${parsedImage.repository}:pull`,
-          },
-        },
-        repository: {
-          url: `${repoUrl}${parsedImage.namespace ? `${parsedImage.namespace}/` : 'library/'}${parsedImage.repository}/manifests/${parsedImage.tag ? parsedImage.tag : 'latest'}`,
-        },
-        nonSecure,
-      };
+  // const manifest = await getManifest(options);
 
-      if (username) {
-        options.token.auth = { username, password };
-      }
-
-      return getManifest(options)
-        .then(data => data)
-        .catch((error) => {
-          console.log(`Error: ${error}`);
-        });
-    });
+  // return manifest;
 }
 
 module.exports.getImageManifest = getImageManifest;
